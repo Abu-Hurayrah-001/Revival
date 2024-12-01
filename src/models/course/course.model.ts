3a32756f18073cfd80052bcc6bce94fb154b07f5
dedic <sahilakhtar5059@gmail.com>
2024-12-01 23:32:49 +0530

    Simplified Course Model

--- src/models/course/course.model.ts
+++ src/models/course/course.model.ts
@@ -2,41 +2,90 @@
 import mongoose, { Schema, Document } from "mongoose";
 
 // INTERFACES
-export interface ICourseCard extends Document {
-    title: string;
-    thumbnailUrl?: string;
-    forWhom: string;
-    startsOn: string;
-    endsOn: string;
-    originalPrice: number;
-    discountedPrice?: number;
+export interface IClasses extends Document {
+    subject: string;
+    chapter: string;
+    videoUrl: string;
+    resourceUrls?: [string];
 };
 
-export interface ICourseDescription extends Document {
+export interface IFAQ extends Document {
+    question: string;
+    answer: string;
+};
+
+export interface ICourse extends Document {
     title: string;
-    
+    batchIncludes: string;
+    description: string;
+    FAQ: [IFAQ]
+    thumbnailUrl: string;
+    introVideoUrl: string;
+    forWhom: string;
+    originalPrice: number;
+    discountedPrice?: number;
+    classes: [IClasses];
+    feedbacks?: [string];
 };
 
 // SCHEMAS
-const courseCardSchema = new Schema<ICourseCard>({
+const classesSchema = new Schema<IClasses>({
+    subject: {
+        type: String,
+        required: true,
+    },
+    videoUrl: {
+        type: String,
+        required: true,
+    },
+    resourceUrls: {
+        type: [String],
+        required: false,
+    },
+});
+
+const FAQSchema = new Schema<IFAQ>({
+    question: {
+        type: String,
+        required: true,
+    },
+    answer: {
+        type: String,
+        required: true,
+    },
+});
+
+const courseSchema = new Schema<ICourse>({
     title: {
+        type: String,
+        required: true,
+    },
+    batchIncludes: {
+        type: String,
+        required: true,
+    },
+    description: {
         type: String,
         required: true,
     },
     thumbnailUrl: {
         type: String,
-        required: false, 
+        required: true, 
+    },
+    introVideoUrl: {
+        type: String,
+        required: true, 
     },
     forWhom: {
         type: String,
         required: true,
     },
-    startsOn: {
-        type: String,
+    classes: {
+        type: [classesSchema],
         required: true,
     },
-    endsOn: {
-        type: String,
+    FAQ: {
+        type: [FAQSchema],
         required: true,
     },
     originalPrice: {
@@ -47,11 +96,11 @@ const courseCardSchema = new Schema<ICourseCard>({
         type: Number,
         required: false,
     },
+    feedbacks: {
+        type: [String],
+        required: false,
+    },
 });
-
-const courseDescriptionSchema = new Schema<ICourseDescription>({
-    
-}, { timestamps: true });
 
 //EXPORT
 export default mongoose.model<ICourse>("Course", courseSchema);
